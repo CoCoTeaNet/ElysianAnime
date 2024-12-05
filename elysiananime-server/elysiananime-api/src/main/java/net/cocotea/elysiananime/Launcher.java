4e94eba14759fb9a76fc4f50722822316d9499cb
CoCoTea <572315466@qq.com>
2024-12-05 16:55:19 +0800

    新增启动banner展示效果

--- elysiananime-server/elysiananime-api/src/main/java/net/cocotea/elysiananime/Launcher.java
+++ elysiananime-server/elysiananime-api/src/main/java/net/cocotea/elysiananime/Launcher.java
@@ -19,15 +19,26 @@ import org.slf4j.LoggerFactory;
 @EnableScheduling
 @Import(scanPackages = {"net.cocotea.elysiananime"})
 public class Launcher {
-    private static final Logger logger = LoggerFactory.getLogger(Launcher.class);
+    private static final Logger log = LoggerFactory.getLogger(Launcher.class);
 
     public static void main(String[] args) {
         SolonApp app = Solon.start(Launcher.class, args);
         AppContext context = app.context();
 
         DefaultProp defaultProp = context.getBean("defaultProp");
-        logger.warn("强密码：{}, 权限缓存状态：{}", defaultProp.getStrongPassword(), defaultProp.getPermissionCache());
+        log.warn("强密码：{}, 权限缓存状态：{}", defaultProp.getStrongPassword(), defaultProp.getPermissionCache());
 
         GlobalConst.START_TIME = System.currentTimeMillis();
+
+        log.info(""" 
+                 ________  __                    _                        _                 _                     \s
+                |_   __  |[  |                  (_)                      / \\               (_)                    \s
+                  | |_ \\_| | |   _   __  .--.   __   ,--.   _ .--.      / _ \\     _ .--.   __   _ .--..--.  .---. \s
+                  |  _| _  | |  [ \\ [  ]( (`\\] [  | `'_\\ : [ `.-. |    / ___ \\   [ `.-. | [  | [ `.-. .-. |/ /__\\\\\s
+                 _| |__/ | | |   \\ '/ /  `'.'.  | | // | |, | | | |  _/ /   \\ \\_  | | | |  | |  | | | | | || \\__.,\s
+                |________|[___][\\_:  /  [\\__) )[___]\\'-;__/[___||__]|____| |____|[___||__][___][___||__||__]'.__.'\s
+                                \\__.'                                                                             \s
+                                                         \s
+                start success!""");
     }
 }
