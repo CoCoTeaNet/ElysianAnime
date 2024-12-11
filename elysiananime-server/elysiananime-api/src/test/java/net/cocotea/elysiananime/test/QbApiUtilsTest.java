2d4407dcfe3ed293a6e673e7055fbb6fdcdc9acb
CoCoTea <572315466@qq.com>
2024-12-11 17:37:25 +0800

    添加单元测试

--- elysiananime-server/elysiananime-api/src/test/java/net/cocotea/elysiananime/test/QbApiUtilsTest.java
+++ elysiananime-server/elysiananime-api/src/test/java/net/cocotea/elysiananime/test/QbApiUtilsTest.java
@@ -1,11 +1,16 @@
 package net.cocotea.elysiananime.test;
 
+import cn.hutool.core.bean.BeanUtil;
+import com.alibaba.fastjson.JSONArray;
+import net.cocotea.elysiananime.api.anime.rss.model.QbInfo;
 import net.cocotea.elysiananime.util.QbApiUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.noear.solon.annotation.Import;
 import org.noear.solon.annotation.Inject;
 import org.noear.solon.test.SolonJUnit4ClassRunner;
+
+import java.io.File;
 
 @Import(scanPackages = {"net.cocotea.elysiananime"})
 @RunWith(SolonJUnit4ClassRunner.class)
@@ -18,6 +23,24 @@ public class QbApiUtilsTest {
     public void test() {
         String cookie = qbApiUtils.login();
         System.out.println(cookie);
+    }
+
+    @Test
+    public void addNewTorrent() {
+        String btUrl = "https://mikanime.tv/Download/20241208/ce47f85b4240a4f630f6eda5c257cad424287a1f.torrent";
+        String savePath = "D:\\data\\qb_download";
+
+        String msg = qbApiUtils.addNewTorrent(btUrl, savePath);
+        System.out.println(msg);
+    }
+
+    @Test
+    public void listTorrent() {
+        JSONArray all = qbApiUtils.info("all");
+        for (Object obj : all) {
+            QbInfo qbInfo = BeanUtil.toBean(obj, QbInfo.class);
+            System.out.println(qbInfo);
+        }
     }
 
 }
